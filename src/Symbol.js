db9e9f6e2d949410af2a5c3ba3de7df8f1edfd57
orangemug <info@orangemug.co.uk>
2020-07-23 14:29:28 +0100

    Remove explict symbol width/height.

--- src/Symbol.js
+++ src/Symbol.js
@@ -63,9 +63,8 @@ function renderTextSymbol ({expr, layer}) {
   return {
     element: "svg",
     attributes: {
-      width: 20,
-      height: 20,
-      viewBox: "0 0 20 20"
+      viewBox: "0 0 20 20",
+      xmlns: "http://www.w3.org/2000/svg"
     },
     children: [
       {
